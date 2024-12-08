07fa522cc9a7ea5d53d4f240438891f6d659fd75
Mohamed2002-G3 <mohamedelsankary14@gmail.com>
2024-12-08 12:46:07 +0200

    finish DetailsCharacter

--- lib/presentation/screens/character_screen.dart
+++ lib/presentation/screens/character_screen.dart
@@ -5,7 +5,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:loading_animation_widget/loading_animation_widget.dart';
 import 'package:rick_and_morty/business_logic/cubit/character_cubit.dart';
 import 'package:rick_and_morty/constant/colors.dart';
-import 'package:rick_and_morty/presentation/widgets/details_widget.dart';
+import 'package:rick_and_morty/presentation/widgets/details_character_widget.dart';
 
 class CharacterScreen extends StatelessWidget {
   const CharacterScreen({super.key});
@@ -14,6 +14,7 @@ class CharacterScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        forceMaterialTransparency: false,
         automaticallyImplyLeading: false,
         backgroundColor: AppColors.yellow,
         title: const Text(
@@ -25,41 +26,65 @@ class CharacterScreen extends StatelessWidget {
         ),
       ),
       backgroundColor: AppColors.grey,
-      body: Padding(
-        padding: const EdgeInsets.only(top: 10, left: 10, right: 10),
-        child: BlocBuilder<CharacterCubit, CharacterState>(
-          builder: (context, state) {
-            if (state is CharactersSuccess) {
-              log('ازيك يبيبي انا جيت');
-              return GridView.builder(
-                physics: const BouncingScrollPhysics(),
-                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 2,
-                  crossAxisSpacing: 10,
-                  mainAxisSpacing: 10,
-                  childAspectRatio: 2 / 3,
-                ),
-                itemCount: state.characterModel.length,
-                itemBuilder: (context, index) => DetailsWidget(
-                  results: state.characterModel[index],
-                ),
-              );
-            }
-            if (state is CharactersFailuer) {
-              return Text(state.errorMassage);
-            }
-            {
-              log('وه جاااار تحميل الداتا');
-              return Center(
-                child: LoadingAnimationWidget.twistingDots(
-                    leftDotColor: AppColors.white,
-                    rightDotColor: AppColors.yellow,
-                    size: 200),
-              );
-            }
-          },
-        ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+            child: TextField(
+              decoration: InputDecoration(
+                suffixIcon: const Icon(Icons.search),
+                enabledBorder: outLineinputBorder(),
+                border: outLineinputBorder(),
+                fillColor: Colors.amber,
+                filled: true,
+                hintText: 'Find a Character ...',
+              ),
+            ),
+          ),
+          BlocBuilder<CharacterCubit, CharacterState>(
+            builder: (context, state) {
+              if (state is CharactersSuccess) {
+                log('ازيك يبيبي انا جيت');
+                return Expanded(
+                  child: GridView.builder(
+                    physics: const BouncingScrollPhysics(),
+                    gridDelegate:
+                        const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2,
+                      crossAxisSpacing: 10,
+                      mainAxisSpacing: 10,
+                      childAspectRatio: 2 / 3,
+                    ),
+                    itemCount: state.characterModel.length,
+                    itemBuilder: (context, index) => DetailsCharacterWidget(
+                      results: state.characterModel[index],
+                    ),
+                  ),
+                );
+              }
+              if (state is CharactersFailuer) {
+                return Text(state.errorMassage);
+              }
+              {
+                log('وه جاااار تحميل الداتا');
+                return Center(
+                  child: LoadingAnimationWidget.twistingDots(
+                      leftDotColor: AppColors.white,
+                      rightDotColor: AppColors.yellow,
+                      size: 200),
+                );
+              }
+            },
+          ),
+        ],
       ),
+    );
+  }
+
+  OutlineInputBorder outLineinputBorder() {
+    return OutlineInputBorder(
+      borderSide: const BorderSide(color: AppColors.yellow),
+      borderRadius: BorderRadius.circular(8),
     );
   }
 }
--- lib/presentation/widgets/details_widget.dart
+++ lib/presentation/widgets/details_widget.dart
@@ -1,61 +1,47 @@
-import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
-import 'package:loading_animation_widget/loading_animation_widget.dart';
 import 'package:rick_and_morty/constant/colors.dart';
 import 'package:rick_and_morty/data/models/results_model.dart';
-import 'package:rick_and_morty/presentation/screens/detail_screen.dart';
 
 class DetailsWidget extends StatelessWidget {
   final Results results;
-  const DetailsWidget({super.key, required this.results});
+  final String title;
+  final String value;
+  final double endIndent;
+  const DetailsWidget(
+      {super.key,
+      required this.title,
+      required this.value,
+      required this.endIndent,
+      required this.results});
+
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () => Navigator.of(context).push(
-        MaterialPageRoute(
-          builder: (context) => DetailScreen(
-            results: results,
-          ),
-        ),
-      ),
-      child: ClipRRect(
-        borderRadius: BorderRadius.circular(8),
-        child: Container(
-          decoration: const BoxDecoration(
-            color: AppColors.white,
-          ),
-          child: GridTile(
-            footer: Container(
-              decoration: const BoxDecoration(
-                color: Colors.black54,
-              ),
-              child: Text(
-                results.name!,
-                maxLines: 2,
-                overflow: TextOverflow.ellipsis,
-                style: const TextStyle(
-                  color: AppColors.white,
-                  fontSize: 15,
-                  fontWeight: FontWeight.bold,
-                ),
-                textAlign: TextAlign.center,
+    return Column(
+      children: [
+        Row(
+          children: [
+            Text(
+              '$title : ',
+              style: const TextStyle(
+                fontSize: 18,
+                color: Colors.white,
               ),
             ),
-            child: CachedNetworkImage(
-              imageUrl: results.image!,
-              fit: BoxFit.cover,
-              placeholder: (context, url) => Center(
-                child: LoadingAnimationWidget.twistingDots(
-                  leftDotColor: AppColors.grey,
-                  rightDotColor: AppColors.yellow,
-                  size: 50,
-                ),
+            Text(
+              value,
+              style: const TextStyle(
+                fontSize: 16,
+                color: AppColors.white,
               ),
-              errorWidget: (context, url, error) => const Icon(Icons.error),
             ),
-          ),
+          ],
         ),
-      ),
+        Divider(
+          endIndent: endIndent,
+          thickness: 2,
+          color: AppColors.yellow,
+        )
+      ],
     );
   }
 }
